b763f87f2dfb735a29c59949e3d0268ab9cc244d
Nasir <nasirhossenmollah@gmail.com>
2025-07-14 00:18:28 +0600

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,20 @@ import Navbar from "./components/Navbar";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import RoadmapItems from "./components/RoadmapItems";
+import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar brandName="Roadmap"/>
+        <Navbar brandName="Roadmap" />
         <Routes>
           <Route path="/" element={<RoadmapItems />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer mt-5 p-10 text-center bg-[#f5f5f5] sm:text-lg">
+      <div>
+        <div>&copy;2025 Nasir Hossen &mdash; All Rights Reserved.</div>
+      </div>
+    </div>
+  );
+}
